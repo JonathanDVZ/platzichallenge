86b07f4d6da437e15307e0bbff2472833e5f7138
Jonathan Villegas <jodaviza@gmail.com>
2023-07-17 01:35:08 -0300

    Format

--- src/client/components/__tests__/atoms/Image.test.tsx
+++ src/client/components/__tests__/atoms/Image.test.tsx
@@ -4,23 +4,23 @@ import { render } from '@testing-library/react';
 import Image, { IImage } from '../../atoms/Image';
 
 describe('components/atoms/Image', () => {
-    test('render Image', () => {
-        const component = render(<Image />);
-        expect(component).toBeVisible;
-    });
+  test('render Image', () => {
+    const component = render(<Image />);
+    expect(component).toBeVisible;
+  });
 
-    test('check entered attributes', () => {
-        const id = 'image-id';
-        const src =
+  test('check entered attributes', () => {
+    const id = 'image-id';
+    const src =
       'https://res.cloudinary.com/dglqojivj/image/upload/v1682559694/simpsons/200px-Jasper_Beardsley_a0zhga.png';
-        const props: IImage = {
-            id,
-            src
-        };
+    const props: IImage = {
+      id,
+      src
+    };
 
-        const { container } = render(<Image {...props} />);
-        const input = container.querySelector('image');
+    const { container } = render(<Image {...props} />);
+    const input = container.querySelector('image');
 
-        expect(input?.getAttribute('id') === id && input?.getAttribute('src') === src).toBeTruthy;
-    });
+    expect(input?.getAttribute('id') === id && input?.getAttribute('src') === src).toBeTruthy;
+  });
 });
