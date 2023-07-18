25e039679512a1fd4c7e8360745e1bb0fa6ea7ac
Jonathan Villegas <jodaviza@gmail.com>
2023-07-18 03:18:06 -0300

    Update spinner

--- src/client/components/atoms/Spinner.tsx
+++ src/client/components/atoms/Spinner.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { DotSpinner } from '@uiball/loaders';
 
 const Spinner: React.FC = () => (
   <div className="ptz-spinner">
-    <DotSpinner size={70} color="#231F20" />
+    <span className="ptz-spinner__spinner"></span>
   </div>
 );
 
--- src/client/views/Home.tsx
+++ src/client/views/Home.tsx
@@ -2,7 +2,6 @@ import React, { useContext } from 'react';
 import CharacterContext from '../context/characters/CharactersContext';
 import CharactersSection from '../components/organisms/CharactersSection';
 import Spinner from '../components/atoms/Spinner';
-import { DotSpinner } from '@uiball/loaders';
 
 const Home: React.FC = () => {
   const {
